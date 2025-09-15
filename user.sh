c02da97d15ba9ca4723690eb72c4c44be8a720da
Kyle Taylor Lange <kyletaylorlange@gmail.com>
2025-09-14 17:28:31 -0700

    Add Flatpaks

--- user.sh
+++ user.sh
@@ -2,3 +2,9 @@
 
 # This file contains commands to be run after rebooting the computer after reinstall and logging in.
 plasma-apply-lookandfeel -a org.kde.breezedark.desktop
+flatpak install flathub com.discordapp.Discord -y
+flatpak install flathub com.spotify.Client -y
+flatpak install flathub org.gimp.GIMP -y
+flatpak install flathub org.audacityteam.Audacity -y
+flatpak install flathub org.libreoffice.LibreOffice -y
+flatpak install flathub org.kde.kdenlive -y
